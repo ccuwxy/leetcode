42c7e3feeda7e4dc4060cc9090c4e80dc764e009
WangXinyu <wxy990212@126.com>
2021-01-16 23:40:52 +0800

    添加项目文件。

---
+++ T1/T1.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i+1; j < nums.size(); j++)
+            {
+                if (nums.at(i) + nums.at(j) == target)
+                    return {i, j};
+            }
+        }
+        return {};
+    }
+    vector<int> twoSum1(vector<int>& nums, int target) {
+        unordered_map<int, int> hashTable;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            auto it = hashTable.find(target - nums[i]);
+            if (it != hashTable.end())
+            {
+                return { it->second,i };
+            }
+            hashTable[nums[i]] = i;
+        }
+        return {};
+    }
+
+};
+
+int main()
+{
+    Solution s ;
+    vector<int> nums = { 2,7,11,15 };
+    int target = 9;
+    for(int i = 0; i < 2;i++)
+        cout << s.twoSum1(nums, target).at(i) << " ";
+    return 0;
+}
